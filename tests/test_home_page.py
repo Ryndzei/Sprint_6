7313edd3915146a057ec7382c8620772e543a7d6
Anton Rineyskiy <arineyskiy@yandex.ru>
2025-04-18 14:41:17 +0300

    test_home_page.py file edited

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,6 +1,6 @@
 import allure
 from page_objects.home_page import HomePage
-from page_objects.base_page import BasePage
+from page_objects.order_page import OrderPage
 from urls import *
 
 
@@ -8,19 +8,18 @@ class TestHomePageLogo:
 
     @allure.title('Проверка на успешный редирект на страницу Дзена по нажатию на слово Яндекс в логотипе')
     def test_successful_redirect_to_dzen_on_click_yandex_logo(self, driver):
-        driver.get(BASE_URL)
         home_page = HomePage(driver)
-        base_page = BasePage(driver)
-        base_page.click_yandex_logo()
+        home_page.open(BASE_URL)
+        home_page.click_yandex_logo()
         home_page.switch_to_new_window()
         home_page.wait_until_url_contains_dzen()
 
-        assert DZEN_URL in driver.current_url, "Не произошёл редирект на Dzen"
+        assert DZEN_URL in home_page.get_current_url(), "Не произошёл редирект на Dzen"
 
     @allure.title('Проверка на успешный редирект на главную страницу Я.Самоката по нажатию на слово Самокат в логотипе')
     def test_successful_redirect_to_main_page_on_click_scooter_logo(self, driver):
-        driver.get(ORDER_URL)
-        base_page = BasePage(driver)
-        base_page.click_scooter_logo()
+        order_page = OrderPage(driver)
+        order_page.open(ORDER_URL)
+        order_page.click_scooter_logo()
 
-        assert BASE_URL == driver.current_url, "Не произошёл переход на главную страницу самоката"
+        assert BASE_URL == order_page.get_current_url(), "Не произошёл переход на главную страницу самоката"
